88f09ee698c45b4609d8f03208ef00e577244611
Daniel Berkowitz <danberko@umich.edu>
2015-04-13 19:14:18 -0400

    Initial Commit

---
+++ chip8/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  chip8
+//
+//  Created by Daniel Berkowitz on 4/13/15.
+//  Copyright (c) 2015 Daniel Berkowitz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
