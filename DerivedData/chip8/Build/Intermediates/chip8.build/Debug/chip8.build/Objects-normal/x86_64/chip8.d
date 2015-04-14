9161bc3ae815b60f50eed1326728532b85239d26
Daniel Berkowitz <danberko@umich.edu>
2015-04-14 12:00:18 -0400

    fixing fopen

---
+++ DerivedData/chip8/Build/Intermediates/chip8.build/Debug/chip8.build/Objects-normal/x86_64/chip8.d
@@ -0,0 +1,2 @@
+dependencies: /Users/dberko/Documents/chip8/chip8/chip8/chip8.cpp \
+  /Users/dberko/Documents/chip8/chip8/chip8/chip8.h
---
+++ DerivedData/chip8/Build/Intermediates/chip8.build/Debug/chip8.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,2 @@
+dependencies: /Users/dberko/Documents/chip8/chip8/chip8/main.cpp \
+  /Users/dberko/Documents/chip8/chip8/chip8/chip8.h
