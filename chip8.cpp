e1752b84889d13e95b0b4c169bbb5994d18b38ad
Daniel Berkowitz <dberkowitz@berkpc.com>
2015-12-07 00:10:41 -0500

    Cleaning

--- chip8.cpp
+++ chip8.cpp
@@ -107,9 +107,9 @@ void chip8::emu() {
                 break;
                 
             case 0x000E: // 0x00EE: Returns from subroutine
-                --sp;			// 16 levels of stack, decrease stack pointer to prevent overwrite
-                pc = stack[sp];	// Put the stored return address from the stack back into the program counter
-                pc += 2;		// Don't forget to increase the program counter!
+                --sp;			
+                pc = stack[sp];
+                pc += 2;		
                 break;
                 
             default:
@@ -122,9 +122,9 @@ void chip8::emu() {
             break;
             
         case 0x2000: // 0x2NNN: Calls subroutine at NNN.
-            stack[sp] = pc;			// Store current address in stack
-            ++sp;					// Increment stack pointer
-            pc = opcode & 0x0FFF;	// Set the program counter to the address at NNN
+            stack[sp] = pc;			
+            ++sp;					
+            pc = opcode & 0x0FFF;
             break;
             
         case 0x3000: // 0x3XNN: Skips the next instruction if VX equals NN
@@ -247,10 +247,6 @@ void chip8::emu() {
             break;
             
         case 0xD000: // DXYN: Draws a sprite at coordinate (VX, VY) that has a width of 8 pixels and a height of N pixels.
-            // Each row of 8 pixels is read as bit-coded starting from memory location I;
-            // I value doesn't change after the execution of this instruction.
-            // VF is set to 1 if any screen pixels are flipped from set to unset when the sprite is drawn,
-            // and to 0 if that doesn't happen
         {
             unsigned short x = v[(opcode & 0x0F00) >> 8];
             unsigned short y = v[(opcode & 0x00F0) >> 4];
